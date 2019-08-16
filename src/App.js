904f2e408ec0bb44f475cd25cd6faa4b79518419
Tim Shur <tim.shur@gmail.com>
2019-08-15 22:31:43 -0700

    frontend: restyle App component to have state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () =>
-  <div className='app'>
-    <h1>Jūrne</h1>
-    <p>
-      Welcome ~ :) We are really happy to have you here.
-      Learn more about our services and see other details
-      on our website.
-    </p>
-    <p>
-      Click the button below to Lock/Unlock your car.
-    </p>
-    <div className='button'>Click me!</div>
-  </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      isLocked: true
+    };
+  }
+
+  render() {
+    const { isLocked } = this.state;
+
+    return (
+      <div className='app'>
+        <h1>Jūrne</h1>
+        <p>
+          Welcome ~ :) We are really happy to have you here.
+          Learn more about our services and see other details
+          on our website.
+        </p>
+        <p>
+          Click the button below to Lock/Unlock your car.
+        </p>
+        <div className='button'
+        >
+          {isLocked ? 'Unlock' : 'Lock'}
+        </div>
+      </div>
+    );
+  }
+
+}
 
 export default App;
