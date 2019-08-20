9b07ea07e8285e7a93012adf64f1de7c46af6d19
tshur <tim.shur@gmail.com>
2019-08-20 14:52:46 -0700

    Navigation: create basic nav component

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const App = () =>
-  <div>
-    App
-  </div>
+import Navigation from '../Navigation';
+
+const App = () => (
+  <Router>
+    <Navigation />
+  </Router>
+);
 
 export default App;
