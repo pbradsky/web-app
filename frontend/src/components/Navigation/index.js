4517ac81b3162b2f9988352424dc3e21486c7cc4
tshur <tim.shur@gmail.com>
2019-08-20 16:34:30 -0700

    consume auth session state in nav component

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import SignOutButton from '../SignOut';
+
+import { AuthUserContext } from '../Session';
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = ({ authUser }) => (
-    <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+const Navigation = () => (
+    <div>
+        <AuthUserContext.Consumer>
+            {authUser =>
+                authUser ? <NavigationAuth /> : <NavigationNonAuth />
+            }
+        </AuthUserContext.Consumer>
+    </div>
 );
 
 const NavigationAuth = () => (
