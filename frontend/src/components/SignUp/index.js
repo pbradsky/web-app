5357bdc81fad7d7fb4b13b07795f2b36db011fab
tshur <tim.shur@gmail.com>
2019-08-21 22:43:05 -0700

    firebase db navigation

--- frontend/src/components/SignUp/index.js
+++ frontend/src/components/SignUp/index.js
@@ -28,11 +28,19 @@ class SignUpFormBase extends Component {
   }
 
   onSubmit = event => {
-    const { email, passwordOne } = this.state;
+    const { username, email, passwordOne } = this.state;
 
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
+        return this.props.firebase
+          .user(authUser.user.uid)
+          .set({
+            username,
+            email
+          });
+      })
+      .then(() => {
         this.setState({ ...INITIAL_STATE });
         this.props.history.push(ROUTES.HOME);
       })
