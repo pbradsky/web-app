11752d4f4a266b2d5b8a2bf02f9239a14efa41d1
tshur <tim.shur@gmail.com>
2019-08-25 16:17:10 -0700

    add basic vehicle components

---
+++ frontend/src/components/Vehicle/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Vehicle = () => (
+    <div>
+        Vehicle!
+    </div>
+);
+
+export default Vehicle;
---
+++ frontend/src/components/VehicleList/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VehicleList = () => (
+    <div>
+        Vehicle List!
+    </div>
+);
+
+export default VehicleList;
