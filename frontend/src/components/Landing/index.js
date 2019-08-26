de2734fa923325a27004ffe70cf0d493dcc3b2b2
tshur <tim.shur@gmail.com>
2019-08-25 20:34:25 -0700

    fix landing page routing

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -1,50 +1,23 @@
-import React, { Component } from 'react';
-import { compose } from 'recompose';
-import { withRouter } from 'react-router-dom';
-
-import AuthUserContext from '../Session/context';
-import { withFirebase } from '../Firebase';
+import React from 'react';
 import { SignInButton } from '../SignIn';
 import { SignUpButton } from '../SignUp';
-import * as ROUTES from '../../constants/routes';
 
-class Landing extends Component {
-  componentDidMount() {
-    this.listener = this.props.firebase.onAuthUserListener(
-      authUser => !!authUser && this.props.history.push(ROUTES.DRIVE)
-    )
-  }
+const Landing = () => (
+  <div>
+    <h1>Jurne</h1>
+    <p>
+      Welcome! We are happy to see you here. Learn more about
+      our product on our website!
+    </p>
+    <br />
+    <p>
+      Please log into your account below or sign up if you don't
+      have an account!
+    </p>
 
-  componentWillUnmount() {
-    this.listener();
-  }
+    <SignInButton />
+    <SignUpButton />
+  </div>
+);
 
-  render() {
-    return (
-      <AuthUserContext.Consumer>
-        {authUser => !!authUser ?
-          <div>
-            <h1>Jūrne</h1>
-            <p>
-              Welcome! We are happy to see you here. Learn more about
-              our product on our website!
-            </p>
-            <br />
-            <p>
-              Please log into your account below or sign up if you don't
-              have an account!
-            </p>
-
-            <SignInButton />
-            <SignUpButton />
-          </div>
-        : null}
-      </AuthUserContext.Consumer>
-    );
-  }
-}
-
-export default compose(
-  withRouter,
-  withFirebase
-)(Landing);
+export default Landing;
