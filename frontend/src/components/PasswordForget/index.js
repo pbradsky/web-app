eb7b3045c43263e3480edcb9be7ce1931444a69c
tshur <tim.shur@gmail.com>
2019-08-27 00:04:00 -0700

    undo protect pw forget page with auth

--- frontend/src/components/PasswordForget/index.js
+++ frontend/src/components/PasswordForget/index.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import { withAuthorization } from '../Session';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
-import * as CONDITIONS from '../../constants/conditions';
 
 const PasswordForgetPage = () => (
   <div>
@@ -74,7 +72,7 @@ const PasswordForgetLink = () => (
   </p>
 );
 
-export default withAuthorization(CONDITIONS.isSignedInUser)(PasswordForgetPage);
+export default PasswordForgetPage;
 
 const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
 
