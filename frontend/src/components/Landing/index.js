be5cc6d25ff33d51423a33ead609b49faee247ae
David Shur <david@thejurne.com>
2019-09-17 12:41:30 -0700

    position text fix logo

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -5,16 +5,17 @@ import { SignUpButton } from '../SignUp';
 import './index.css';
 
 const Landing = () => (
-  <div className="right">
-    <h1 className="left">JURNE</h1>
+  <div className="outer">
+    <h1 className="inner">JURNE
+    <h1 className="slogan"> EXTRAORDINARY. EVERYDAY.</h1>
+    </h1>
+
     <p >
       Residential Car-Share
     </p>
-    <br />
     <p >
       Unlock with the Push of a Button
     </p>
-    <br />
     <p >
       Helping Cars and Cities be Friends Again
     </p>
