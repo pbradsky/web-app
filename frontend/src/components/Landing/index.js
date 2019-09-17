c4625fab8aa2f7673f2791036d5b3f15c7fa5710
David Shur <david@thejurne.com>
2019-09-16 23:44:11 -0700

    add style to landing page

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { SignInButton } from '../SignIn';
 import { SignUpButton } from '../SignUp';
 
+import './index.css';
+
 const Landing = () => (
-  <div>
+  <div className='right'>
     <h1>Jurne</h1>
-    <p>
+    <p className='body-primary'>
       Welcome! We are happy to see you here. Learn more about
       our product on our website!
     </p>
     <br />
-    <p>
+    <p className='body-secondary'>
       Please log into your account below or sign up if you don't
       have an account!
     </p>
