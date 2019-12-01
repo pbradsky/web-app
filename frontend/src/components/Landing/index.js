9568221d36dc0950a282de7e89d7d24950785f71
David <david@thejurne.com>
2019-11-30 20:20:29 -0800

    make landing look real-life status

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import { SignInButton } from '../SignIn';
 import { JoinNowButton } from '../SignUp';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import Alert from 'react-bootstrap/Alert';
 
 const Landing = () => (
-  <div className='right'>
-    <h1>Jurne</h1>
-    <p className='body-primary'>
-      Welcome! We are happy to see you here. Learn more about
-      our product on our website!
-    </p>
+  <Container>
     <br />
-    <p className='body-secondary'>
-      Please log into your account below or sign up if you don't
-      have an account!
-    </p>
+    <Alert variant='info' dismissible>
+      Note: We are currently undergoing construction!
+    </Alert>
+    <Jumbotron>
+        <h1>Welcome</h1>
+        <br />
+        <p className='body-primary'>
+          We are happy to see you here.
+        </p>
 
-  </div>
+        <p className='body-secondary'>
+          Please log in to your account, or sign up if you don't
+          have one yet.
+        </p>
+    </Jumbotron>
+    <Card>
+      <Card.Header>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>People are our purpose.</Card.Title>
+        <Card.Text>
+          If you are interested in joining our family, click the subscribe button below.
+          Doing that will add you to our mailing list, where you will hear all about what's up-and-coming!
+          <br />Together we will do great things.
+        </Card.Text>
+      </Card.Body>
+      <Button variant="outline-primary">Subscribe</Button>
+    </Card>
+    <br />
+  </Container>
 );
 
 export default Landing;
