3573c425adb99c6822afd18e0e56308ff41f9b63
David <david@thejurne.com>
2019-12-03 01:33:03 -0800

    refactor code fix nav errors

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -26,7 +26,7 @@ const Navigation = () => (
 );
 
 const NavigationAuth = ({ authUser }) => (
-    <Navbar collapseOnSelect expand='lg' className='border-bottom' bg='white'>
+    <Navbar collapseOnSelect expand='md' className='border-bottom' bg='white'>
         <Link to={ROUTES.LANDING}>
             <img
                 src='spread.png'
@@ -37,18 +37,27 @@ const NavigationAuth = ({ authUser }) => (
         <Navbar.Toggle aria-controls='responsive-navbar-nav' style={{border: '0px', outline: 'none'}} />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className='ml-auto'>
-                <Nav.Link>
-                    <Link style={{textDecoration: 'none', color: 'inherit'}} to={ROUTES.LANDING}>Home</Link>
-                </Nav.Link>
-            {!!authUser.roles[ROLES.ADMIN] && (
-                <Nav.Link>
-                    <Link style={{textDecoration: 'none', color: 'inherit' }} to={ROUTES.ADMIN}>Admin</Link>
-                </Nav.Link>
-            )}
-                <Nav.Link>
-                    <Link style={{textDecoration: 'none', color: 'inherit' }} to={ROUTES.ACCOUNT}>Account</Link>
-                </Nav.Link>
-                <SignOutButton />
+                    <Link
+                        className='nav-link'
+                        style={{textDecoration: 'none', color: 'muted'}}
+                        to={ROUTES.LANDING}>
+                            Home
+                    </Link>
+                {!!authUser.roles[ROLES.ADMIN] && (
+                    <Link
+                        className='nav-link'
+                        style={{textDecoration: 'none', color: 'muted' }}
+                        to={ROUTES.ADMIN}>
+                            Admin
+                    </Link>
+                )}
+                    <Link
+                        className='nav-link'
+                        style={{textDecoration: 'none', color: 'muted' }}
+                        to={ROUTES.ACCOUNT}>
+                            Account
+                    </Link>
+                    <SignOutButton />
             </Nav>
         </Navbar.Collapse>
     </Navbar>
@@ -56,25 +65,24 @@ const NavigationAuth = ({ authUser }) => (
 
 const NavigationNonAuth = () => (
     <Navbar className='border-bottom' bg='white'>
-
-        <Navbar.Brand>
-            <Link to={ROUTES.LANDING}>
-                <img
-                    src='spread.png'
-                    height='50'
-                    alt='jurne logo'
-                    className='d-inline-block align-top'
-                />
+        <Link to={ROUTES.LANDING}>
+            <img
+                src='spread.png'
+                height='50'
+                alt='jurne logo'
+                className='d-inline-block align-top'
+            />
+        </Link>
+        <Nav className='ml-auto'>
+            <Link
+                className='nav-link'
+                style={{textDecoration: 'none', color: 'muted'}}
+                to={ROUTES.LANDING}>
+                    Home
             </Link>
-        </Navbar.Brand>
-
-        <Nav className='ml-auto pr-2'>
-            <Nav.Link><Link style={{textDecoration: 'none', color: 'inherit'}} to={ROUTES.LANDING}>Home</Link></Nav.Link>
         </Nav>
-
         <SignInButton />
         <JoinNowButton />
-
     </Navbar>
 );
 
