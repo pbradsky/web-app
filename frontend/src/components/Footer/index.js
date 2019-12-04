6681c1f5aacf6c99d92692727233c4eff5be05dc
David <david@thejurne.com>
2019-12-04 12:26:23 -0800

    style footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -8,22 +8,25 @@ import * as ROUTES from '../../constants/routes';
 const copyright = '\u00a9';
 
 const Footer = () => (
-  <Navbar bg='white' className='border-top mt-5'>
+  <Navbar bg='white' className='border-top' style={{justifyContent: 'center'}}>
     <Link className='nav-link'
+          style={{fontSize: '10px', color: '#7f7f7f'}}
           to={ROUTES.TERMS}>
       Terms
     </Link>
     <Link className='nav-link'
+          style={{fontSize: '10px', color: '#7f7f7f'}}
           to={ROUTES.PRIVACY}>
       Privacy
     </Link>
     <Link className='nav-link'
+          style={{fontSize: '10px', color: '#7f7f7f'}}
           to={ROUTES.CONTACT}>
       Contact
     </Link>
-    <Navbar.Text className='ml-auto mr-auto'
-                 style={{fontSize: '10px'}}>
-      {copyright} 2019 Jurne Inc. All Rights Reserved
+    <Navbar.Text className='nav-link'
+          style={{fontSize: '10px'}}>
+      {copyright} 2019 Jurne Inc.
     </Navbar.Text>
   </Navbar>
 );
