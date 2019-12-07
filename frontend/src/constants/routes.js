3d7d68f5461858fab4459e61f15db5ca97ee8764
tshur <tim.shur@gmail.com>
2019-12-06 21:23:42 -0800

    fix merge conflicts

--- frontend/src/constants/routes.js
+++ frontend/src/constants/routes.js
@@ -2,13 +2,7 @@ export const LANDING = '/';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
 export const CONTRACT = '/contract';
-<<<<<<< HEAD
 export const CONFIRMATION = '/confirmation';
-export const DRIVE = '/drive';
-export const DRIVE_VEHICLE = '/drive/:id';
-=======
-export const HOLDING = '/holding';
->>>>>>> 22f929802b9c52b1de24bd49c5a0bdfe6be901ad
 export const ACCOUNT = '/account';
 export const ADMIN = '/admin';
 export const PASSWORD_FORGET = '/pw-forget';
