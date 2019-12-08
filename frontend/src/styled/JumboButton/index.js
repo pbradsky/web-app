d62d85ecc94eaa7612561a40efbe77db49a08df3
David <david@thejurne.com>
2019-12-08 01:27:04 -0800

    add jumbobutton and bg for landing

---
+++ frontend/src/styled/JumboButton/index.js
@@ -0,0 +1,10 @@
+import BootstrapButton from 'react-bootstrap/Button';
+import styled from 'styled-components';
+
+const JumboButton = styled(BootstrapButton)`
+    &:hover {
+      color: #27A745;
+    }
+`;
+
+export default JumboButton;
