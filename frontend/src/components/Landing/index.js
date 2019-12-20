cd3c20ba8989576b9ff665d265ec67b0c695c425
David <david@thejurne.com>
2019-12-19 22:43:21 -0800

    make card its own element landing page

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -9,6 +9,23 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 
 import * as ROUTES from 'constants/routes';
 
+const ContactCard = () => (
+  <Card>
+    <Card.Body>
+      <Card.Title>Reach out</Card.Title>
+      <hr />
+      <Card.Text>
+        If you have feedback,
+        click the button below to get in touch.
+      </Card.Text>
+      <Card.Text>
+        We're here to listen.
+      </Card.Text>
+      <Link to={ROUTES.CONTACT}><Button variant='outline-primary'>Contact Us</Button></Link>
+    </Card.Body>
+  </Card>
+)
+
 const Landing = () => (
   <Container>
     <Jumbotron>
@@ -16,20 +33,7 @@ const Landing = () => (
       <p>Time is precious. Why waste it at a desk?</p>
     </Jumbotron>
     <CardDeck>
-      <Card>
-        <Card.Body>
-          <Card.Title>Reach out</Card.Title>
-          <hr />
-          <Card.Text>
-            If you have feedback,
-            click the button below to get in touch.
-          </Card.Text>
-          <Card.Text>
-            We're here to listen.
-          </Card.Text>
-          <Link to={ROUTES.CONTACT}><Button variant='outline-primary'>Contact Us</Button></Link>
-        </Card.Body>
-      </Card>
+      <ContactCard />
     </CardDeck>
   </Container>
 );
