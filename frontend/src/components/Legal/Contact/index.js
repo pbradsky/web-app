e0b364dc286cb1269597a27a55ae68c201c87d4f
David <david@thejurne.com>
2019-12-20 12:00:17 -0800

    refactor contact us page

--- frontend/src/components/Legal/Contact/index.js
+++ frontend/src/components/Legal/Contact/index.js
@@ -20,6 +20,31 @@ const ContactPage = () => (
   </Container>
 );
 
+const ContactCard = () => (
+  <Card>
+    <Card.Body>
+      <Card.Title>Reach out</Card.Title>
+      <hr />
+      <Card.Text>
+        If you have feedback,
+        click the button below to get in touch.
+      </Card.Text>
+      <Card.Text>
+        We're here to listen.
+      </Card.Text>
+      <ContactUsButton />
+    </Card.Body>
+  </Card>
+);
+
+const ContactUsButton = () => (
+  <Link to={ROUTES.CONTACT}>
+    <Button variant='outline-primary'>
+      Contact Us
+    </Button>
+  </Link>
+);
+
 const INITIAL_STATE = {
   name: '',
   email: '',
@@ -57,69 +82,47 @@ class ContactFormBase extends Component {
 
     return (
       <Form onSubmit={this.onSubmit}>
-        <Form.Label>Full Name</Form.Label>
-        <Form.Control
-          name='name'
-          value={name}
-          onChange={this.onChange}
-          type='text'
-          placeholder='Full Name'
-        />
-        <br />
-        <Form.Label>Email Address</Form.Label>
-        <Form.Control
-          name='email'
-          value={email}
-          onChange={this.onChange}
-          type='text'
-          placeholder='Email Address'
-        />
-        <br />
+        <Form.Group>
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control
+            name='name'
+            value={name}
+            onChange={this.onChange}
+            type='text'
+            placeholder='Full Name'
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            name='email'
+            value={email}
+            onChange={this.onChange}
+            type='text'
+            placeholder='Email Address'
+          />
+        </Form.Group>
+        <Form.Group>
         <Form.Label>Message</Form.Label>
-        <Form.Control
-          name='message'
-          value={message}
-          onChange={this.onChange}
-          type='text'
-          placeholder='Message (500 character limit)'
-          as='textarea'
-          rows='3'
-        />
-        <br />
+          <Form.Control
+            name='message'
+            value={message}
+            onChange={this.onChange}
+            type='text'
+            placeholder='Message (500 character limit)'
+            as='textarea'
+            rows='3'
+          />
+        </Form.Group>
         <Button disabled={isInvalid} type='submit'>
           Send
         </Button>
       </Form>
-    )
+    );
   }
 }
-
-const ContactUsButton = () => (
-  <Link to={ROUTES.CONTACT}>
-    <Button variant='outline-primary'>
-      Contact Us
-    </Button>
-  </Link>
-);
-
-const ContactCard = () => (
-  <Card>
-    <Card.Body>
-      <Card.Title>Reach out</Card.Title>
-      <hr />
-      <Card.Text>
-        If you have feedback,
-        click the button below to get in touch.
-      </Card.Text>
-      <Card.Text>
-        We're here to listen.
-      </Card.Text>
-      <ContactUsButton />
-    </Card.Body>
-  </Card>
-);
 
 const ContactForm = withRouter(ContactFormBase);
 
 export default ContactPage;
-export { ContactCard };
+export { ContactCard, ContactUsButton };
