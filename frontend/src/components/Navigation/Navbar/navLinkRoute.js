466b0f3996ee51c87b25973f975eb2094980c8bb
David <david@thejurne.com>
2020-01-13 13:26:12 -0800

    line remove

--- frontend/src/components/Navigation/Navbar/navLinkRoute.js
+++ frontend/src/components/Navigation/Navbar/navLinkRoute.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
-
 import styled from 'styled-components';
 
 const Styled = styled.div`
   display: flex;
   align-items: center;
-`
+`;
 
 const NavLinkRoute = ({show, ...props}) => {
   const showLink = show == null || show;
