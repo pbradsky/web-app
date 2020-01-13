d2840fce4d2905dfcc85baf46156bd41af087215
David <david@thejurne.com>
2020-01-13 11:29:25 -0800

    keep Footer styling

--- frontend/src/components/Navigation/Footer/index.js
+++ frontend/src/components/Navigation/Footer/index.js
@@ -7,7 +7,6 @@ import * as ROUTES from 'constants/routes';
 
 const copyright = '\u00a9';
 
-<<<<<<< HEAD
 const Styles = styled.div`
   .navbar {
     justify-content: center;
@@ -43,30 +42,6 @@ const Footer = () => (
       <p>{copyright} {new Date().getFullYear()} Jurne Inc.</p>
     </Navbar>
   </Styles>
-=======
-const Footer = () => (
-  <Navbar bg='white' style={{justifyContent: 'center'}}>
-    <Link className='nav-link'
-          style={{fontSize: '10px', color: '#7f7f7f'}}
-          to={ROUTES.TERMS}>
-      Terms
-    </Link>
-    <Link className='nav-link'
-          style={{fontSize: '10px', color: '#7f7f7f'}}
-          to={ROUTES.PRIVACY}>
-      Privacy
-    </Link>
-    <Link className='nav-link'
-          style={{fontSize: '10px', color: '#7f7f7f'}}
-          to={ROUTES.CONTACT}>
-      Contact
-    </Link>
-    <Navbar.Text className='nav-link'
-          style={{fontSize: '10px'}}>
-      {copyright} {new Date().getFullYear()} Jurne Inc.
-    </Navbar.Text>
-  </Navbar>
->>>>>>> 58b31cea6c5a925411ef6ed819e0d936fb8520df
 );
 
 export default Footer;
