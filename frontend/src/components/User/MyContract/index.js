882efb1c64f5c39a97b42a1e5afa7a2d2307c39e
tshur <tim.shur@gmail.com>
2020-01-16 18:28:56 -0800

    fill MyContract page

--- frontend/src/components/User/MyContract/index.js
+++ frontend/src/components/User/MyContract/index.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import { compose } from 'recompose';
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+import {
+  PreambleText,
+  ContractFormText,
+  SignedUserText
+} from 'components/Registration/Contract/text';
 
 import { withAuthorization, withUser } from 'api/Session';
+import formatAddress from 'utils/address';
 import * as CONDITIONS from 'constants/conditions';
 
-const MyContractPage = ({ authUser }) => (
-  <Container>
-    <Jumbotron>
-      <h1>My Contract</h1>
-      <p>{authUser.email}</p>
-      <hr />
-    </Jumbotron>
-  </Container>
-);
+const MyContractPage = ({ authUser }) => {
+  const { email, fullName, phone, license, contract } = authUser;
+  const fullAddress = formatAddress(authUser);
+
+  return (
+    <Container>
+      <Jumbotron>
+        <h1>My Signed Contract</h1>
+        <p>{email}</p>
+      </Jumbotron>
+      <Card style={{overflowY: 'scroll', height: '50vh'}}>
+        <Card.Body>
+          <ContractFormText
+            name={fullName}
+            address={fullAddress}
+            phone={phone}
+            license={license}
+            />
+          <PreambleText />
+          <SignedUserText
+            contract={contract}
+            />
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+}
 
 export default compose(
   withUser,
