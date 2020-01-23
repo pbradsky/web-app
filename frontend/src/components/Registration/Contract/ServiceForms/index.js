3800f39d8f2fe2329ebfb576ac774c5eaedbf61f
David <david@thejurne.com>
2020-01-23 12:08:32 -0800

    update service index to reflect correct data fields

---
+++ frontend/src/components/Registration/Contract/ServiceForms/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import NotesFormStage from './notes';
+import MultiStageForm from '../MultiStageForm';
+import SignatureFormStage from './signature';
+
+import * as ROUTES from 'constants/routes';
+
+const forms = [
+  { ...VehicleFormStage },
+  { ...NotesFormStage },
+  { ...SignatureFormStage },
+];
+
+const FinalizeContractPage = ({ history }) => {
+  const onSubmit = forms => {
+    const { out, due, vin, plate, year, make, model,
+            color, milesAllowed, mileageOut, rate, fuel, notes} = forms[0].state;
+    const { signature, date } = forms[1].state;
+
+    // TODO(tim): add db publish
+    console.log({
+      dbData: {
+        vehicleInfo: {
+          out, due, vin,
+          plate, year, make,
+          model, color, milesAllowed,
+          mileageOut, rate, fuel,
+          notes
+        },
+        contract: {
+          signature,
+          date
+        },
+      }
+    });
+
+    // TODO(tim): update route
+  }
+
+  return (
+    <MultiStageForm
+      title='Finalize User Contract'
+      forms={forms}
+      onSubmit={onSubmit} />
+  );
+};
+
+export default withRouter(FinalizeContractPage);
