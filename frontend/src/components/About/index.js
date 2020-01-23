ed3afb193b76a8c9927a63711d8a213df2e65881
David <david@thejurne.com>
2020-01-23 10:41:20 -0800

    page scroll on button click

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -3,13 +3,12 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-import bgimage from 'assets/jumboImage.jpeg';
 import steps from 'assets/jurne-steps.png';
 
 import styled from 'styled-components';
 
 const BackdropImage = styled.div`
-    background-image: url(${bgimage});
+    background-color: #f7f7f7;
     background-size: cover;
     background-repeat: no-repeat;
     background-position: bottom;
@@ -18,15 +17,15 @@ const BackdropImage = styled.div`
     flex-direction: column;
 `;
 
-const AboutPage = () => (
-  <BackdropImage>
-   <div className='mt-auto mb-auto p-4'>
+const AboutPage = React.forwardRef((props, ref) => (
+  <BackdropImage ref={ref}>
+    <div className='m-auto w-75'>
       <Row>
         <Col className='p-0'>
           <img className='p-0 col-12' src={steps} alt='jurne easy process' />
         </Col>
       </Row>
-      <Row className='text-white text-center mt-4 mb-auto'>
+      <Row className='text-center mt-4 mb-auto'>
         <Col className='col-3'><p>Make an Account</p></Col>
         <Col className='col-3'><p>Complete our Form</p></Col>
         <Col className='col-3'><p>Finalize with Dealership</p></Col>
@@ -34,7 +33,6 @@ const AboutPage = () => (
       </Row>
     </div>
   </BackdropImage>
-
-);
+));
 
 export default AboutPage;
--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from 'react-bootstrap/Button';
 import bgimage from 'assets/jumboImage.jpeg';
 
 import styled from 'styled-components';
+import AboutPage from 'components/About';
 
 import * as ROUTES from 'constants/routes';
 
@@ -20,29 +21,49 @@ const BackdropImage = styled.div`
     text-align: center;
 `;
 
-const AboutUsButton = () => (
-  <div className='mt-auto mb-4'>
-    <Link to={ROUTES.ABOUT}>
-      <Button variant='outline-secondary'>
-        Learn More
-      </Button>
-    </Link>
-  </div>
-);
+class Landing extends Component {
+  constructor(props) {
+     super(props)
+     this.state = {
+        field: 1,
+     }
+     //creates a reference for your element to use
+     this.myDivToFocus = React.createRef()
+  }
 
-const Landing = () => (
-  <BackdropImage>
-    <div className='mt-auto'>
-      <h1 className='text-white'>Your Drive Starts Here</h1>
-      <p className='text-white'>The easiest way to test-drive.</p>
-      <Link to={ROUTES.SIGN_UP}>
-        <Button size='lg'>
-          Test Drive Today
-        </Button>
-      </Link>
-    </div>
-    <AboutUsButton />
-  </BackdropImage>
-);
+  handleOnClick = (event) => {
+      //.current is verification that your element has rendered
+      if(this.myDivToFocus.current){
+          this.myDivToFocus.current.scrollIntoView({
+             behavior: "smooth",
+             block: "nearest"
+          })
+      }
+  }
+
+  render() {
+    return (
+      <>
+        <BackdropImage>
+          <div className='mt-auto'>
+            <h1 className='text-white'>Your Drive Starts Here</h1>
+            <p className='text-white'>The easiest way to test-drive.</p>
+            <Link to={ROUTES.SIGN_UP}>
+              <Button size='lg'>
+                Test Drive Today
+              </Button>
+            </Link>
+          </div>
+          <div className="mt-auto mb-4">
+            <Button onClick={this.handleOnClick} variant='outline-secondary'>
+              Learn More
+            </Button>
+          </div>
+        </BackdropImage>
+        <AboutPage ref={this.myDivToFocus}/>
+      </>
+    )
+  }
+}
 
 export default Landing;
