cc1138b01176b48595f5f1886e3d67dce4ee7e83
Tim Shur <tim.shur@gmail.com>
2020-01-23 21:26:05 -0800

    add basic service/loaner page

--- frontend/src/components/User/Dashboard/Service/index.js
+++ frontend/src/components/User/Dashboard/Service/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const ServicePage = () => (
-  <div>Service</div>
-);
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+const ServicePage = () => {
+  const addLoaner = () => {
+    // TODO(tim): add form to get loaner vehicle information and add to db
+    //     using a modal/popup?
+    console.log('add loaner vehicle');
+  }
+
+  return (
+    <Container>
+      <Jumbotron>
+        <h1>Service Partner Dashboard</h1>
+        <p>This page is only accessible to our Dealership Service partners.</p>
+      </Jumbotron>
+      <div>
+        Loaner Fleet
+        {/* TODO(tim): add loaner fleet list component */}
+      </div>
+      <Button onClick={addLoaner}>New</Button>
+    </Container>
+  );
+};
 
 export default ServicePage;
