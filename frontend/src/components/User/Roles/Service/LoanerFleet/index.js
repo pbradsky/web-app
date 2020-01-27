69c6a9d268331ed16512a1e3abb8c41b6b853f46
Tim Shur <tim.shur@gmail.com>
2020-01-26 21:43:32 -0800

    add search to loaner list

--- frontend/src/components/User/Roles/Service/LoanerFleet/index.js
+++ frontend/src/components/User/Roles/Service/LoanerFleet/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { compose } from 'recompose';
+import Fuse from 'fuse.js';
 
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 import LoanerList from './list';
+import { WithPageLoad } from 'components/Util/Loading';
+import Search from 'components/Util/Search';
+
+import { withAuthorization } from 'api/Session';
+import { withFirebase } from 'api/Firebase';
+import { vehicleSearchOptions } from 'utils/search';
+
+import * as CONDITIONS from 'constants/conditions';
+import * as ROLES from 'constants/roles';
 
 // This is part of the Service Dept. Suite
 // 1. Vehicles can be added/removed from the list.
@@ -13,28 +25,88 @@ import LoanerList from './list';
 // 5. Search on this page looks for vehicles by Make, Model, VIN, status.
 // 6. This page updates the dropdown menu and autofill on Loaner Form.
 // Vehicle sshould have VIN, Make, Model, Year, Color, License, Status, Mileage
-const LoanerFleetPage = () => {
-  const vehicles = [
-    {
-      vin: '123VIN456ABC',
-      make: 'Honda',
-      model: 'Civic',
-      year: '2002',
-      color: 'gray',
-      license: 'L1C3N53',
-      status: 'available'
-    }
-  ];
+class LoanerFleetPage extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Container>
-      <Jumbotron>
-        <h1>Loaner Fleet Dashboard</h1>
-      </Jumbotron>
-      <br />
-      <LoanerList vehicles={vehicles} />
-    </Container>
-  );
-};
+    const vehicles = [
+      {
+        vin: '123VIN456ABC',
+        make: 'Honda',
+        model: 'Civic',
+        year: '2002',
+        color: 'gray',
+        license: 'L1C3N53',
+        status: 'available'
+      }
+    ];
+
+    this.state = {
+      fuse: new Fuse(vehicles, vehicleSearchOptions),
+      vehicles: vehicles,
+      loading: false,
+      searchQuery: '',
+    };
+  }
+
+  componentDidMount() {
+    // this.setState({ loading: true });
+
+    // this.props.firebase.users().on('value', snapshot => {
+    //   const usersObject = snapshot.val();
+
+    //   const usersList = Object.keys(usersObject).map(key => ({
+    //     ...usersObject[key],
+    //     uid: key
+    //   })).filter(CONDITIONS.isDealerViewable);
+
+    //   this.setState({
+    //     fuse: new Fuse(usersList, userSearchOptions),
+    //     users: usersList,
+    //     loading: false,
+    //   });
+    // });
+  }
+
+  componentWillUnmount() {
+    // this.props.firebase.users().off();
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  addLoaner = () => {
+    // TODO(tim): add form to get loaner vehicle information and add to db
+    //     using a modal/popup?
+    console.log('add loaner vehicle');
+  }
+
+  render() {
+    const { loading, vehicles, searchQuery, fuse } = this.state;
+
+    const searchedVehicles = searchQuery
+      ? fuse.search(searchQuery)
+      : vehicles;
+
+    return (
+      <Container>
+        <WithPageLoad loading={loading}>
+          <Jumbotron>
+            <h1>Loaner Fleet Dashboard</h1>
+          </Jumbotron>
+          <div>
+            Loaner Fleet
+            {/* TODO(tim): add loaner fleet list component */}
+          </div>
+          <Button onClick={this.addLoaner}>New</Button>
+          <Search searchQuery={searchQuery} onChange={this.onChange} />
+          <br />
+          <LoanerList vehicles={searchedVehicles} />
+        </WithPageLoad>
+      </Container>
+    );
+  }
+}
 
 export default LoanerFleetPage;
--- frontend/src/utils/search.js
+++ frontend/src/utils/search.js
@@ -13,4 +13,20 @@ const userSearchOptions = {
   ],
 };
 
-export { userSearchOptions };
+const vehicleSearchOptions = {
+  shouldSort: true,
+  threshold: 0.2,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 0,
+  keys: [
+    'vin',
+    'make',
+    'model',
+    'status',
+    'license'
+  ],
+}
+
+export { userSearchOptions, vehicleSearchOptions };
